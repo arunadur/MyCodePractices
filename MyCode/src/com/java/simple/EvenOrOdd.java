82aa28ac5afd082f96d9410f5664e1506a1e5365
aruna <aruna@LAPTOP-42HJU9VR>
2020-10-05 12:21:31 +0530

    made SOP changes

--- MyCode/src/com/java/simple/EvenOrOdd.java
+++ MyCode/src/com/java/simple/EvenOrOdd.java
@@ -16,12 +16,13 @@ public class EvenOrOdd {
 	 */
 	public static void main(String[] args) {
 
-		int num = new Scanner(System.in).nextInt();
-		System.out.println("Enter the integer to check even or odd "+num);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the integer to check even or odd ");
+		int num = scan.nextInt();
 		if(num%2==0) {
-			System.out.println(num+ "is an even");
+			System.out.println(num+ " is an even");
 		}else {
-			System.out.println(num+ "is an odd");
+			System.out.println(num+ " is an odd");
 		}
 	}
 
